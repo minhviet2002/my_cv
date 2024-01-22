4de65ad5b6df3ea498ac61de4e59773542ad02d6
minhviet2002 <94736097+minhviet2002@users.noreply.github.com>
2024-01-22 11:08:23 +0700

    update

--- src/js/index.js
+++ src/js/index.js
@@ -57,3 +57,23 @@ items.forEach((item, i) => {
     }
   };
 });
+const section = document.querySelectorAll(".section");
+var sections = {};
+var i = 0;
+
+Array.prototype.forEach.call(section, function (e) {
+  sections[e.id] = e.offsetTop;
+});
+
+window.onscroll = function () {
+  var scrollPosition =
+    document.documentElement.scrollTop || document.body.scrollTop;
+  for (i in sections) {
+    if (sections[i] <= scrollPosition) {
+      document
+        .querySelector(".menu-item-link.active")
+        .classList.remove("active");
+      document.querySelector('a[href="#' + i + '"]').classList.add("active");
+    }
+  }
+};
